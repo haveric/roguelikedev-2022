a25fa0c4eb4e911335fd8810f6f9d6480b52f6e3
Ryan Breuer <breuerryan@gmail.com>
2026-08-05 19:19:50 -0500

    Fix quotes for eslint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: "development",
+    devtool: "inline-source-map",
     devServer: {
         static: {
             directory: path.resolve(__dirname, "dist"),
